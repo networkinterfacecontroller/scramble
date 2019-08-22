27ca48c8ad1b0549dc358a3286092bf1439906e3
networkinterfacecontroller <ntwrkctrl@zoho.com>
2019-08-22 13:05:22 -0600

    token generation

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
-from flask import Flask
+import os
 
+from flask import Flask, request, jsonify
+from twilio.jwt.access_token import AccessToken
+from twilio.jwt.access_token.grants import ChatGrant
+from dotenv import load_dotenv, find_dotenv
 
-app.route('/')
+load_dotenv(find_dotenv())
+app = Flask(__name__)
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+api_key = os.environ['TWILIO_API_KEY']
+api_secret = os.environ['TWILIO_API_SECRET']
+chat_service_sid = os.environ['TWILIO_CHAT_SERVICE_SID']
+
+@app.route('/')
 def index():
     pass
 
-app.route('/token')
+@app.route('/token', methods=['POST','GET'])
 def token():
+    if request.form.get('identity'):
+        token = AccessToken(account_sid, api_key, api_secret, identity=request.form['identity'])
+        chat_grant = ChatGrant(service_sid=chat_service_sid)
+        token.add_grant(chat_grant)
+        return jsonify({'token': token.to_jwt().decode('utf-8')})
+    else:
+        return 'tokens need an identity', 400
     pass
 
-app.route('/login')
+@app.route('/login')
 def login():
     pass
+
+app.run(debug=True, host='0.0.0.0', port=8888)
